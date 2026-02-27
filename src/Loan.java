2585892474bc261e6d77ab1a150fe5062477ad46
Dylan Fernandes <87030593+dylbobuloid@users.noreply.github.com>
2026-02-27 23:27:08 +0000

    Added attributes for loan class

--- src/Loan.java
+++ src/Loan.java
@@ -1,3 +1,15 @@
+import java.util.ArrayList;
 public class Loan {
+    private String name;
+    private  int id;
+    private ArrayList<Book> booksBorrowed = new ArrayList<>();
+
+    public Loan(String name, int id){
+        this.name = name;
+        this.id = id;
+
+    }
+
+
 
 }
